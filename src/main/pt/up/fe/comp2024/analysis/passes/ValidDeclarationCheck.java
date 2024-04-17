6a2a39953e5c2e16e227a94d34ccdf9c0ef79817
Antonio Rego <antoniocostarego618@gmail.com>
2024-04-17 13:35:33 +0100

    fix declaring invalid types for variable

---
+++ src/main/pt/up/fe/comp2024/analysis/passes/ValidDeclarationCheck.java
@@ -0,0 +1,52 @@
+package pt.up.fe.comp2024.analysis.passes;
+
+import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
+import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.report.Report;
+import pt.up.fe.comp.jmm.report.Stage;
+import pt.up.fe.comp2024.analysis.AnalysisVisitor;
+import pt.up.fe.comp2024.ast.Kind;
+import pt.up.fe.comp2024.ast.NodeUtils;
+import pt.up.fe.specs.util.SpecsCheck;
+
+/**
+ * Checks if the type of the expression in a return statement is compatible with the method return type.
+ *
+ * @author JBispo
+ */
+public class ValidDeclarationCheck extends AnalysisVisitor {
+
+    private String currentMethod;
+
+    @Override
+    public void buildVisitor() {
+        addVisit(Kind.METHOD_DECL, this::visitMethodDecl);
+        addVisit(Kind.VAR_DECL, this::visitVarDecl);
+    }
+
+    private Void visitMethodDecl(JmmNode method, SymbolTable table) {
+        currentMethod = method.get("name");
+        return null;
+    }
+
+    private Void visitVarDecl(JmmNode varDecl, SymbolTable table) {
+        SpecsCheck.checkNotNull(currentMethod, () -> "Expected current method to be set");
+
+        // Check if exists a parameter or variable declaration with the same name as the variable reference
+        if (!(varDecl.getChild(0).getKind().equals(Kind.CUSTOM_TYPE.toString()) || varDecl.getChild(0).getKind().equals(Kind.ARRAY_TYPE.toString()) || varDecl.getChild(0).getKind().equals(Kind.PRIMITIVE_TYPE.toString()))){
+            var message = String.format("Invalid type for variable declaration '%s'", varDecl.getChild(0).getKind());
+            addReport(Report.newError(
+                    Stage.SEMANTIC,
+                    NodeUtils.getLine(varDecl),
+                    NodeUtils.getColumn(varDecl),
+                    message,
+                    null)
+            );
+            return null;
+        }
+
+        return null;
+    }
+
+
+}
