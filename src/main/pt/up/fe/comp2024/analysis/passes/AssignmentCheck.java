46dfef4cc3be7ad5b70cea0171cce19ac7515f23
Antonio Rego <antoniocostarego618@gmail.com>
2024-04-19 20:07:14 +0100

    fix for lhs assignment

--- src/main/pt/up/fe/comp2024/analysis/passes/AssignmentCheck.java
+++ src/main/pt/up/fe/comp2024/analysis/passes/AssignmentCheck.java
@@ -56,8 +56,19 @@ public class AssignmentCheck extends AnalysisVisitor {
                 return null;
             }
         }
-        else if (assign.getChild(0).getKind().equals(Kind.ARRAY_ACCESS.toString())){
+        else if (assign.getChild(0).getKind().equals(Kind.ARRAY_ACCESS.toString()) && assign.getChild(0).getChild(0).getKind().equals(Kind.VAR_REF_EXPR.toString())){
             varRefName = assign.getChild(0).getChild(0).get("name");
+            if (varRefName.equals("this")){
+                var message = "Assignment to 'this' on the left hand side.";
+                addReport(Report.newError(
+                        Stage.SEMANTIC,
+                        NodeUtils.getLine(assign),
+                        NodeUtils.getColumn(assign),
+                        message,
+                        null)
+                );
+                return null;
+            }
             forceNotArray = true;
         }
         else {
